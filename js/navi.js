b6830deaa7ac6ffd31f451d6759e0a9d363b1608
CeeDeeLe <c.deten@gmx.de>
2023-12-08 15:41:37 +0100

    Navi auf Div, aber Seite hakt beim Laden

--- js/navi.js
+++ js/navi.js
@@ -1,12 +1,37 @@
 // responsive Nav-Bar
+
+// ###################
+// ursprünglicher Code
+// function BuergerNav() {
+//   let x = document.getElementById("drop-item");
+//   let logo;
+//   if (x.className === "drop-item") {
+//     x.className += " responsive";
+//     // logo = document.getElementById("logo").style.display = "none";
+//   } else {
+//     x.className = "drop-item";
+//     // logo = document.getElementById("logo").style.display = "flex";
+//   }
+// }
+
+// ###################
+// https://www.w3schools.com/howto/howto_js_dropdown.asp
+/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
 function BuergerNav() {
-  var x = document.getElementById("topnav");
-  let logo;
-  if (x.className === "topnav") {
-    x.className += " responsive";
-    logo = document.getElementById("logo").style.display = "none";
-  } else {
-    x.className = "topnav";
-    logo = document.getElementById("logo").style.display = "flex";
-  }
+  document.getElementById("drop-item").classList.toggle("responsive");
 }
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".nav-icon")) {
+    let dropdowns = document.getElementsByClassName("drop-item");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      console.log(i);
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("responsive")) {
+        openDropdown.classList.remove("responsive");
+      }
+    }
+  }
+};
